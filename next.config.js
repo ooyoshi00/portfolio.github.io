1c5de8a26cbba5643172747a5e5151ca41f4a7b9
yoshioka <yosshioka0022@gmail.com>
2024-01-28 22:59:55 +0900

    imageを最適化させないようにする

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 
 module.exports = withNextra({
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
+  images: {
+    // MEMO: https://nextjs.org/docs/messages/export-image-api
+    unoptimized: true
+  }
 })
