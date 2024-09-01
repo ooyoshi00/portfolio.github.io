25d80a9a6f84f61f4aec013ca0f555bd757ba0e1
yoshioka <yosshioka0022@gmail.com>
2024-09-01 21:54:03 +0900

    AboutページのUIを修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 'use client'
 
 import {
+  Avatar,
   Box,
   List,
   ListItem,
   ListItemIcon,
   ListItemText,
+  Paper,
   Typography
 } from '@mui/material'
 import CheckIcon from '@mui/icons-material/Check'
+import { grey } from '@mui/material/colors'
 
 const profile = {
-  name: 'Yoshi Oka'
+  name: 'Yoshioka'
 }
 
 const career = [
@@ -23,29 +26,30 @@ const career = [
   '運動するのが好きで、中学から野球を続けてます'
 ]
 
+const startYear = 2020
 const Home = () => {
+  const workYear = new Date().getFullYear() - startYear
   return (
     <>
       <Box display={'flex'} flexDirection="column" justifyContent={'center'}>
         <Typography variant="h2" margin={2}>
           About
         </Typography>
-        <Typography variant="h3" margin={2}>
-          {profile.name}
-        </Typography>
-        <Box display={'flex'} flexDirection={'column'} margin={2}>
-          <Typography variant="h4">経歴</Typography>
-          <List>
-            {career.map((item) => (
-              <ListItem>
-                <ListItemIcon>
-                  <CheckIcon fontSize="small" sx={{ color: 'black' }} />
-                </ListItemIcon>
-                <ListItemText primary={item} />
-              </ListItem>
-            ))}
-          </List>
-        </Box>
+        <Paper elevation={1} sx={{ padding: '2rem' }}>
+          <Typography variant="h4">{profile.name}</Typography>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Avatar
+              alt="myAvatar"
+              src={'/images/myIcon.jpeg'}
+              sx={{ width: 80, height: 80 }}
+            />
+            <Box p={2} color={grey[700]}>
+              大学で情報工学を専攻し、新卒でフロントエンドエンジニアとして採用。現在はサーバーサイドの設計・開発にも携わっています。実務
+              {workYear}
+              年目となり、フロントエンドとバックエンドの両面での開発経験を積んでいます。
+            </Box>
+          </Box>
+        </Paper>
       </Box>
     </>
   )
